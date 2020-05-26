f742e70edc9eff5534e84c06699baea90029d7c4
lesgaag <leslievandergaag@gmail.com>
2020-05-26 18:04:16 +0200

    first routing set-up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArrowPersonaComponent } from './arrow-persona/arrow-persona.component';
+import { DetailPersonaComponent } from './detail-persona/detail-persona.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { QuestionaireComponent } from './questionaire/questionaire.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main-component', component: MainPageComponent },
+  { path: 'arrow-component', component: ArrowPersonaComponent },
+  { path: 'detail-component', component: DetailPersonaComponent },
+  { path: 'question-component', component: QuestionaireComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
