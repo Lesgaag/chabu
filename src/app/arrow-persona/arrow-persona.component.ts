0f721f174e6fba9596ede9d49d76df61016756c1
lesgaag <leslievandergaag@gmail.com>
2020-07-29 16:27:01 +0200

    add links

--- src/app/arrow-persona/arrow-persona.component.ts
+++ src/app/arrow-persona/arrow-persona.component.ts
@@ -8,11 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class ArrowPersonaComponent implements OnInit {
 
   // add this data to a json file
-  val = '';
   movieTitle = 'The Lost World: Jurassic Park';
   directorName = 'Steven Spielberg';
   yearOfTheatre = '1997';
   summary = 'The summary of the specific movie';
+  // create components within this component that links to correct quiz
+  linkQuiz = '/question-component';
 
   constructor() { }
 
